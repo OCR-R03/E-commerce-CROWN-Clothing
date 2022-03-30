69cf1482c9d928e5aff67e6964f97a57a8f49c56
Carpenter <contact7841@gmail.com>
2022-03-31 00:10:17 +0200

    ajout du composant de connexion ainsi que des utilitaires firebase et apporté des bibliothèques firebase

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -4,7 +4,9 @@ import {
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
-} from 'firebase/auth'    
+} from 'firebase/auth' 
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {                                                      //      <==== SDK Firebase
     apiKey: "AIzaSyBCFgTWfRO5sWtwqHG77MjgcRNqJVsGaHE",
@@ -26,3 +28,28 @@ const firebaseConfig = {                                                      //
 
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    const userSnapshot = await getDoc(userDocRef)
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth
+        const createAt = new Date()
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createAt
+            })
+        } catch (error) {
+            console.log('error creating the user', error.message)
+        }
+    }
+
+    return userDocRef
+}
